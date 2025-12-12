9ac427b7133b43d2b8d3330ef6fe93f2463604b2
Mr-Singh007 <54988368+Mr-Singh007@users.noreply.github.com>
2025-12-12 17:09:58 +0000

    String File

---
+++ FirstFolder/Strings_Topic.js
@@ -0,0 +1,5 @@
+let myFirstName = "Manveer"
+let myLastName = "Singh"
+let msg = "String literals can be specified using single or double quotes, which are treated identically, or using the backtick character"
+// console.log(`My name is ${myFirstName} ${myLastName}`);
+console.log(msg.split(","));
