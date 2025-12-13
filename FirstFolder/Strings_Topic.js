be276a85102a1053f67ebb05e6448b464748def9
Mr-Singh007 <54988368+Mr-Singh007@users.noreply.github.com>
2025-12-13 16:41:22 +0000

    Strings try

--- FirstFolder/Strings_Topic.js
+++ FirstFolder/Strings_Topic.js
@@ -1,5 +1,5 @@
 let myFirstName = "Manveer"
 let myLastName = "Singh"
-let msg = "String literals can be specified using single or double quotes, which are treated identically, or using the backtick character"
+let msg = "String literals (denoted by double or single quotes) and strings returned from String calls in a non-constructor context (that is, called without using the new keyword) are primitive strings. In contexts where a method is to be invoked on a primitive string or a property lookup occurs, JavaScript will automatically wrap the string primitive and call the method or perform the property lookup on the wrapper object instead."
 // console.log(`My name is ${myFirstName} ${myLastName}`);
-console.log(msg.split(","));
+console.log(msg.length);
